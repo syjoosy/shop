4d2ba57a1949d8cfc9d3e64399e8db48ef114fa8
Alex <vadimnikolaev27@gmail.com>
2020-09-03 23:54:18 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,88 @@
+money = 500
+bag = []
+shop = [
+            ["макароны", 100],
+            ["гречка", 88],
+            ["мясо", 500],
+            ["лимон", 200],
+        ]
+warehouse = []
+
+print("Добро пожаловать в наш магазин")
+name = input("Введите своё имя: ")
+if name == "Админ" or name == "админ" or name == "АДМИН" or name == "аДМИН":
+    print("Добро пожаловать в админ панель: ")
+    print("В магазине имеются товары: ")
+    for array in shop:
+        print(array[0], "-", array[1], 'p')
+    while True:
+        choice_3 = input("Добавить/удалить товар? ")
+        if choice_3 == "Добавить" or choice_3 == "ДОБАВИТЬ" or choice_3 == "добавить":
+            product = input("Введите название продукта: ")
+            price = input("Введите цену: ")
+            warehouse.append(product)
+            warehouse.append(price)
+            shop.append(warehouse)
+        elif choice_3 == "Удалить" or choice_3 == "УДАЛИТЬ" or choice_3 == "удалить":
+            delete = input("Введите продукт, который желаете удалить: ")
+            i = 0
+            for array in shop:
+                i += 1
+                test = array[0]
+                price_delete = array[1]
+                if test == delete:
+                    print(test, " = ",delete)
+                    array.clear
+                    print("Товар ", delete, " удален из магазина")
+                    print(shop)
+                # print(array)
+                # try:
+                #     if shop.index(delete) == 0:
+                #         print(shop.index(delete))
+                # except BaseException:
+                #     continue
+            # warehouse.append("лимон")
+            # print(shop.index(warehouse))
+
+        else:
+            break
+
+
+choice = input("Будете что-нибудь приобретать? ")
+while choice == "ДА" or choice == "да" or choice == "Да" or choice == "дА":
+    
+    print("В магазине имеются товары: ")
+    for array in shop:
+        print(array[0], "-", array[1], 'p')
+
+    print("Сейчас у вас ", money, " рублей")
+    print("В вашей сумке сейчас находятся следующие товары: ")
+    for i in bag:
+        print(i)
+    
+    if money <= 88:
+        print("У вас недостаточно средств для покупки в нашем магазине ")
+        break
+    choice_2 = input("Что будете приобретать? ")
+
+    for array in shop:
+
+        if choice_2 in array[0]:
+
+            if money >= array[1]:
+                money -= array[1]
+                bag.append(array[0])
+                print("С вашего счета списано", array[1], ' рублей в ', array[0], " добавлен в вашу корзину ")
+                continue
+
+            else:
+                print("У вас недостаточно средств для покупки ")
+        
+        elif choice_2 not in array[0]:
+            continue
+
+        else:
+            print("Такого товара в магазине нет! ")
+
+else:
+    print("Выход из программы магазина ")
