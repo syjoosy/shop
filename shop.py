2ff6f28bcf369d8fc00de9e07e232a9dca66db64
syjoosy <64636699+syjoosy@users.noreply.github.com>
2020-09-13 23:44:13 +0300

    Add files via upload

--- shop.py
+++ shop.py
@@ -17,6 +17,13 @@ def add_products(user_group, shop = shop, **products):
 
     return shop
 
+def change(products):
+    for group, array in shop.items():
+        for product, price in array.items():
+            if product == products:
+                price = input("Введите новую цену для продукта {} ".format(product))
+                
+
 def print_all_products(shop=shop):
     for group, array in shop.items():
         print("\nРаздел: " + group)
@@ -26,9 +33,11 @@ def print_all_products(shop=shop):
 while True:
     choice = input("Добавить продукт? ")
     if choice == "Да" or choice == "ДА" or choice == "да" or choice == "дА":
-        category = input("Введите категорию: ")
-        product = input("Введите продукт: ")
-        price = input("Введите цену: ")
-        add_products(category, product = price)
+        pass
+        # category = input("Введите категорию: ")
+        # product = input("Введите продукт: ")
+        # price = input("Введите цену: ")
+        # add_products(input("Введите категорию: "), input("Введите продукт: ") = int(input("Введите цену: ")))
 
+    change(input("Введите продукт: "))
     print_all_products()
