d6cf7367178a7e080910ad8590b23d4103dea0b8
syjoosy <64636699+syjoosy@users.noreply.github.com>
2020-09-14 23:36:07 +0300

    new function

--- shop.py
+++ shop.py
@@ -17,27 +17,53 @@ def add_products(user_group, shop = shop, **products):
 
     return shop
 
+
 def change(products):
     for group, array in shop.items():
         for product, price in array.items():
             if product == products:
                 price = input("Введите новую цену для продукта {} ".format(product))
+                # shop.update(price)
                 
+def remove_product():
+    pass
+
 
 def print_all_products(shop=shop):
     for group, array in shop.items():
         print("\nРаздел: " + group)
         for product, price in array.items():
             print(product + ' - ' + str(price))
-            
-while True:
-    choice = input("Добавить продукт? ")
-    if choice == "Да" or choice == "ДА" or choice == "да" or choice == "дА":
-        pass
+
+
+def add_products_to_section(section, products):
+    if section in shop:
+        shop[section].update(products)
+    # else:
+    #     shop.append(section)
+    #     shop[section].update(products)
+
+products_count = int(input("Введите сколько товаров вы хотите добавить: "))
+products_section = input("Введите название раздела: ")
+user_products = {}
+
+for i in range(products_count):
+    product_name = input("Введите название товара: ")
+    product_price = input("Введите цену товара: ")
+    user_products.update({product_name: product_price})
+
+add_products_to_section(products_section, user_products)
+print(shop)
+
+# while True:
+#     choice = input("Добавить продукт? ")
+#     if choice == "Да" or choice == "ДА" or choice == "да" or choice == "дА":
+#         pass
         # category = input("Введите категорию: ")
         # product = input("Введите продукт: ")
         # price = input("Введите цену: ")
         # add_products(input("Введите категорию: "), input("Введите продукт: ") = int(input("Введите цену: ")))
+        #add_products(category, product = price),
 
-    change(input("Введите продукт: "))
-    print_all_products()
+    # change(input("Введите продукт: "))
+    # print_all_products()
